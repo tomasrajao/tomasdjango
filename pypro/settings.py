eab752c492fdab93d3d9f1d7f67f30cc1d6c7c75
tomasrajao <tomasrajao@gmail.com>
2021-09-23 15:25:46 -0300

    Configurado collectfast

--- pypro/settings.py
+++ pypro/settings.py
@@ -33,6 +33,8 @@ DEBUG = config('DEBUG', cast=bool)
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 
 # Application definition
+STATICFILES_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
+COLLECTFAST_STRATEGY = "collectfast.strategies.boto3.Boto3Strategy"
 
 INSTALLED_APPS = [
     'django.contrib.admin',
