f329a60047605ed953e51e6a9cd7dfecfb32a627
tomasrajao <tomasrajao@gmail.com>
2021-09-24 16:28:33 -0300

    Configurações, correções de código e remoção de variável duplicada

--- pypro/settings.py
+++ pypro/settings.py
@@ -33,8 +33,10 @@ DEBUG = config('DEBUG', cast=bool)
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 
 # Application definition
-STATICFILES_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
-COLLECTFAST_STRATEGY = "collectfast.strategies.boto3.Boto3Strategy"
+
+COLLECTFAST_STRATEGY = 'collectfast.strategies.boto3.Boto3Strategy'
+
+AUTH_USER_MODEL = 'base.User'
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -44,7 +46,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'collectfast',
     'django.contrib.staticfiles',
-    'pypro.base.apps',
+    'pypro.base',
 ]
 
 MIDDLEWARE = [
@@ -150,19 +152,19 @@ if AWS_ACCESS_KEY_ID:
     AWS_DEFAULT_ACL = None
 
     # Static Assets
-    # ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒
+    # ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒ #
     STATICFILES_STORAGE = 's3_folder_storage.s3.StaticStorage'
     STATIC_S3_PATH = 'static'
-    STATIC_ROOT = f'/{STATIC_S3_PATH}'
-    STATIC_URL = f'//s3.amazonws.com/{AWS_STORAGE_BUCKET_NAME}/{STATIC_S3_PATH}'
+    STATIC_ROOT = f'/{STATIC_S3_PATH}/'
+    STATIC_URL = f'//s3.amazonws.com/{AWS_STORAGE_BUCKET_NAME}/{STATIC_S3_PATH}/'
     ADMIN_MEDIA_PREFIX = f'{STATIC_URL}admin/'
 
     # Upload Media Folder
-    # ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒
+    # ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒ #
     DEFAULT_FILE_STORAGE = 's3_folder_storage.s3.DefaultStorage'
     DEFAULT_S3_PATH = 'media'
-    MEDIA_ROOT = f'/{DEFAULT_S3_PATH}'
-    MEDIA_URL = f'//s3.amazonws.com/{AWS_STORAGE_BUCKET_NAME}/{DEFAULT_S3_PATH}'
+    MEDIA_ROOT = f'/{DEFAULT_S3_PATH}/'
+    MEDIA_URL = f'//s3.amazonws.com/{AWS_STORAGE_BUCKET_NAME}/{DEFAULT_S3_PATH}/'
 
     INSTALLED_APPS.append('s3_folder_storage')
     INSTALLED_APPS.append('storages')
