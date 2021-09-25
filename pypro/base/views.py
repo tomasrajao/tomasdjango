fb3a5b3386cfa3438f09c83d714f9a5e21c333ba
tomasrajao <tomasrajao@gmail.com>
2021-09-25 12:03:02 -0300

    Inserido formato html

--- pypro/base/views.py
+++ pypro/base/views.py
@@ -4,4 +4,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return HttpResponse('Olá Django')
+    return HttpResponse('<html><body>Olá Django</body></html>')
