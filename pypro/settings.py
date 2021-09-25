c9ccafe25f3362aee72f0989852ad5dafa4957dc
tomasrajao <tomasrajao@gmail.com>
2021-09-25 10:57:09 -0300

    Alteração storage configuration

--- pypro/settings.py
+++ pypro/settings.py
@@ -150,7 +150,7 @@ if AWS_ACCESS_KEY_ID:
     COLLECTFAST_ENABLED = True
     COLLECTFAST_STRATEGY = 'collectfast.strategies.boto3.Boto3Strategy'
 
-    AWS_DEFAULT_ACL = None
+    AWS_DEFAULT_ACL = 'private'
 
     # Static Assets
     # ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒ #
