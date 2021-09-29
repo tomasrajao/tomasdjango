46508ff2342883fa76da018d365d22aeeefff6bf
tomasrajao <tomasrajao@gmail.com>
2021-09-29 11:56:46 -0300

    Criado teste de índice de vídeos

---
+++ pypro/aperitivos/tests/test_indice.py
@@ -0,0 +1,30 @@
+import pytest
+from django.urls import reverse
+from model_bakery import baker
+
+from pypro.aperitivos.models import Video
+from pypro.django_assertions import assert_contains
+
+@pytest.fixture
+def videos(db):
+    return baker.make(Video, 3)
+
+@pytest.fixture
+def resp(client, videos):
+    resp = client.get(reverse('aperitivos:indice'))
+    return resp
+
+
+def test_status_code(resp):
+    assert resp.status_code == 200
+
+
+def test_titulo_video(resp, videos):
+    for video in videos:
+        assert_contains(resp, video.titulo)
+
+
+def test_link_video(resp, videos):
+    for video in videos:
+        video_link = reverse('aperitivos:video', args=(video.slug,))
+        assert_contains(resp, f'href="{video_link}"')
