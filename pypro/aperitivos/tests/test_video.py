2365a0c5904eebfb14f120dbdc6bd0b2cd04057a
tomasrajao <tomasrajao@gmail.com>
2021-09-29 11:57:41 -0300

    Teste de vídeo com model_bakery

--- pypro/aperitivos/tests/test_video.py
+++ pypro/aperitivos/tests/test_video.py
@@ -1,22 +1,38 @@
 import pytest
 from django.urls import reverse
+from model_bakery import baker
 
+from pypro.aperitivos.models import Video
 from pypro.django_assertions import assert_contains
 
 
 @pytest.fixture
-def resp(client):
-    resp = client.get(reverse('aperitivos:video', args=('motivacao',)))
+def video(db):
+    return baker.make(Video)
+
+
+@pytest.fixture
+def resp(client, video):
+    resp = client.get(reverse('aperitivos:video', args=(video.slug,)))
     return resp
+
+
+@pytest.fixture
+def resp_video_nao_encontrado(client, video):
+    return client.get(reverse('aperitivos:video', args=(f'{video.slug}-video-nao-encontrado',)))
 
 
 def test_status_code(resp):
     assert resp.status_code == 200
 
 
-def test_titulo_video(resp):
-    assert_contains(resp, '>Vídeo Aperitivo: Motivação<h1/>')
+def test_status_code_video_nao_encontrado(resp_video_nao_encontrado):
+    assert resp_video_nao_encontrado.status_code == 404
 
 
-def test_conteudo_video(resp):
-    assert_contains(resp, '<iframe src="https://player.vimeo.com/video/574703144')
+def test_titulo_video(resp, video):
+    assert_contains(resp, video.titulo)
+
+
+def test_conteudo_video(resp, video):
+    assert_contains(resp, f'<iframe src="https://player.vimeo.com/video/{video.vimeo_id}')
