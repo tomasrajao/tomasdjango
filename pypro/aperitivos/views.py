34442ab26855b0494ea42c94b1cb1415e85c8029
tomasrajao <tomasrajao@gmail.com>
2021-09-29 12:00:38 -0300

    Criada view índice

--- pypro/aperitivos/views.py
+++ pypro/aperitivos/views.py
@@ -1,12 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
-# Create your views here.
+from pypro.aperitivos.models import Video
+
+videos = [
+    Video(slug='motivacao', titulo='Vídeo Aperitivo: Motivação', vimeo_id='574703144'),
+    Video(slug='instalacao-windows', titulo='Instalação Windows', vimeo_id='576473838'),
+]
 
 
 def video(request, slug):
-    videos = {
-        'motivacao': {'titulo': 'Vídeo Aperitivo: Motivação', 'vimeo_id': 574703144},
-        'instalacao-windows': {'titulo': 'Instalação Windows', 'vimeo_id': 576473838},
-    }
-    video = videos[slug]
+    video = get_object_or_404(Video, slug=slug)
     return render(request, 'aperitivos/video.html', context={'video': video})
+
+
+def indice(request):
+    videos = Video.objects.order_by('creation').all()
+    return render(request, 'aperitivos/indice.html', context={'videos': videos})
