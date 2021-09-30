9ab80abf173728d620e699fb37cb0553ef3986dd
tomasrajao <tomasrajao@gmail.com>
2021-09-30 15:35:05 -0300

    Incluídas regras de negócio para encontrar módulo e listar aulas de módulo

--- pypro/modulos/facade.py
+++ pypro/modulos/facade.py
@@ -3,5 +3,13 @@ from typing import List
 from pypro.modulos.models import Modulo
 
 
-def listar_modulos_ordenados()-> List[Modulo]:
-    return list(Modulo.objects.order_by('titulo').all())
+def listar_modulos_ordenados() -> List[Modulo]:
+    return list(Modulo.objects.order_by('order').all())
+
+
+def encontrar_modulo(slug: str) -> Modulo:
+    return Modulo.objects.get(slug=slug)
+
+
+def listar_aulas_de_modulo_ordenadas(modulo: Modulo):
+    return list(modulo.aula_set.order_by('order').all())
