4656393e32d95349e84aec2e3188cdc8bf302bd3
tomasrajao <tomasrajao@gmail.com>
2021-09-30 15:35:39 -0300

    Criados modelos para Módulo e Aula

--- pypro/modulos/models.py
+++ pypro/modulos/models.py
@@ -1,5 +1,37 @@
 from django.db import models
 
 # Create your models here.
-class Modulo(models.Model):
+from django.urls import reverse
+from ordered_model.models import OrderedModel
+
+
+class Modulo(OrderedModel):
     titulo = models.CharField(max_length=64)
+    slug = models.SlugField(unique=True)
+    publico = models.TextField()
+    descricao = models.TextField()
+
+    class Meta(OrderedModel.Meta):
+        pass
+
+    def __str__(self):
+        return self.titulo
+
+    def get_absolute_url(self):
+        return reverse('modulos:detalhe', kwargs={'slug': self.slug})
+
+
+class Aula(OrderedModel):
+    titulo = models.CharField(max_length=64)
+    slug = models.SlugField(unique=True)
+    modulo = models.ForeignKey('Modulo', on_delete=models.PROTECT)
+    order_with_respect_to = 'modulo'
+
+    class Meta(OrderedModel.Meta):
+        pass
+
+    def __str__(self):
+        return self.titulo
+
+    def get_absolute_url(self):
+        return reverse('modulos:detalhe', kwargs={'slug': self.slug})
