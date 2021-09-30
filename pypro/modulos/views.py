65fdd51249c4b4d6301aa32b35230173f1954803
tomasrajao <tomasrajao@gmail.com>
2021-09-30 16:02:24 -0300

    Função de view com contexto dos modelos de aulas e modulo

--- pypro/modulos/views.py
+++ pypro/modulos/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+from pypro.modulos import facade
+
+
+def detalhe(request, slug):
+    modulo = facade.encontrar_modulo(slug)
+    aulas = facade.listar_aulas_de_modulo_ordenadas(modulo)
+    return render(request, 'modulos/modulo_detalhe.html', {'modulo': modulo, 'aulas': aulas})
