eb625093dd4863c973d9f58da5c9453f43549f20
tomasrajao <tomasrajao@gmail.com>
2021-10-02 12:53:25 -0300

    Criação de turmas e cadastro de alunos

--- pypro/turmas/admin.py
+++ pypro/turmas/admin.py
@@ -3,11 +3,25 @@ from django.contrib import admin
 # Register your models here.
 from django.contrib.admin import ModelAdmin
 
-from pypro.turmas.models import Turma
+from pypro.turmas.models import Turma, Matricula
+
+
+class MatriculaInline(admin.TabularInline):
+    model = Matricula
+    extra = 0
+    ordering = ('-data',)
+    autocomplete_fields = ('usuario',)
+    readonly_fields = ('data',)
+
+    def has_change_permission(self, request, obj=None):
+        return False
 
 
 @admin.register(Turma)
 class TurmaAdmin(ModelAdmin):
+    inlines = [
+        MatriculaInline,
+    ]
     list_display = ('nome', 'slug', 'inicio', 'fim')
-    prepopulated_fields = {'slug': ('nome',) }
+    prepopulated_fields = {'slug': ('nome',)}
     ordering = ('-inicio',)
