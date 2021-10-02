fed49222357f897a109b0192180252a0a5656538
tomasrajao <tomasrajao@gmail.com>
2021-10-02 10:32:51 -0300

    Criação de turmas e matrículas, e restrição de matrículas únicas por turma

---
+++ pypro/turmas/models.py
@@ -0,0 +1,26 @@
+from django.contrib.auth import get_user_model
+from django.db import models
+
+
+# Create your models here.
+
+
+class Turma(models.Model):
+    nome = models.CharField(max_length=32)
+    slug = models.SlugField(max_length=32)
+    inicio = models.DateField()
+    fim = models.DateField()
+    alunos = models.ManyToManyField(get_user_model(), through='Matricula')
+
+    def __str__(self):
+        return self.nome
+
+
+class Matricula(models.Model):
+    data = models.DateTimeField(auto_now_add=True)
+    usuario = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    turma = models.ForeignKey(Turma, on_delete=models.CASCADE)
+
+    class Meta:
+        unique_together = ['usuario', 'turma']
+        ordering = ['turma', 'data']
