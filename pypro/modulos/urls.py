8490dc0948fef0251828f5c288281518f485123b
tomasrajao <tomasrajao@gmail.com>
2021-10-03 10:16:13 -0300

    Lint de código

--- pypro/modulos/urls.py
+++ pypro/modulos/urls.py
@@ -1,11 +1,10 @@
 from django.urls import path
 
 from pypro.modulos import views
-from pypro.modulos.models import Modulo
 
 app_name = 'modulos'
 urlpatterns = [
     path('', views.indice, name='indice'),
     path('<slug:slug>', views.detalhe, name='detalhe'),
-    path(f'aulas/<slug:slug>', views.aula, name='aula'),
+    path('aulas/<slug:slug>', views.aula, name='aula'),
 ]
