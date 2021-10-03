70a166c9e40469748e166f533bfce30800b02978
tomasrajao <tomasrajao@gmail.com>
2021-10-02 22:51:45 -0300

    Página de login de usuário

--- pypro/urls.py
+++ pypro/urls.py
@@ -21,6 +21,7 @@ from pypro.base.views import home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('contas/', include('django.contrib.auth.urls')),
     path('', include('pypro.base.urls')),
     path('aperitivos/', include('pypro.aperitivos.urls')),
     path('modulos/', include('pypro.modulos.urls')),
