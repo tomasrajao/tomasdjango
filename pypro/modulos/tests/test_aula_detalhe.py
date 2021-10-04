05de628f349e6cae73368e1991804aaa33bcbdd6
tomasrajao <tomasrajao@gmail.com>
2021-10-03 21:20:54 -0300

    Redirecionamento correto de usuários não logados

--- pypro/modulos/tests/test_aula_detalhe.py
+++ pypro/modulos/tests/test_aula_detalhe.py
@@ -17,8 +17,8 @@ def aula(modulo):
 
 
 @pytest.fixture
-def resp(client, aula):
-    resp = client.get(reverse('modulos:aula', kwargs={'slug': aula.slug}))
+def resp(client_usuario_logado, aula):
+    resp = client_usuario_logado.get(reverse('modulos:aula', kwargs={'slug': aula.slug}))
     return resp
 
 
@@ -40,3 +40,15 @@ def test_breadcrumb_modulo(resp, modulo: Modulo):
 
 def test_breadcrumb_aula(resp, aula: Aula):
     assert_contains(resp, f'<li class="breadcrumb-item active" aria-current="page">{aula.titulo}</li>')
+
+
+@pytest.fixture
+def resp_sem_usuario(client, aula):
+    resp = client.get(reverse('modulos:aula', kwargs={'slug': aula.slug}))
+    return resp
+
+
+def test_redirect_usuario_nao_logado(resp_sem_usuario, aula):
+    assert resp_sem_usuario.status_code == 302
+    assert resp_sem_usuario.url.startswith(reverse('login'))
+    assert resp_sem_usuario.url.endswith(reverse('modulos:aula', kwargs={'slug': aula.slug}))
